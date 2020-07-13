7dc4462c998c63d5d91790c58944fa5aef03252b
Tao <taoyu65@gmail.com>
2020-07-13 10:48:11 -0700

    minor

--- web/0623/execute/main4.py
+++ web/0623/execute/main4.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 
 user = pd.read_csv("csv/main2.csv", low_memory=False)
 trade = pd.read_csv("csv/main3.csv", low_memory=False)
@@ -11,9 +12,28 @@ trade = pd.read_csv("csv/main3.csv", low_memory=False)
 db = user.merge(trade, how='left')
 db = db[['user_id', 'buying_currency']]
 
-cny_amount = db[db['buying_currency'] == 'CNY'].count()[0]
-usd_amount = db[db['buying_currency'] == 'USD'].count()[0]
-print(cny_amount)   # count: 238
-print(usd_amount)   # count: 193
+cny = db[db['buying_currency'] == 'CNY']
+usd = db[db['buying_currency'] == 'USD']
+print(cny.count()[0])   # count: 238
+print(usd.count()[0])   # count: 193
 
-db.to_csv('csv/main4.csv')
+# getting final result
+# Group A: (238 / 2 = 119 cny) + (193 / 2 = 96 usd) = 215
+# Group B: (238 / 2 = 119 cny) + (193 / 2 = 97 usd) = 216
+
+cny_list = list(cny['user_id'])
+random.shuffle(cny_list)
+
+usd_list = list(usd['user_id'])
+random.shuffle(usd_list)
+
+group_a = cny_list[:119] + usd_list[:96]
+group_b = cny_list[119:] + usd_list[96:]
+
+print(group_a)
+print(group_b)
+
+# print(cny)
+
+# cny.to_csv('csv/main4_cny.csv')
+# usd.to_csv('csv/main4_usd.csv')
