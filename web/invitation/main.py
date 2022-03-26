1c4fd410a0d8419072defaa5fc8edc6c707de31a
Tao <taoyu65@gmail.com>
2022-03-25 17:03:35 -0700

    minor

--- web/invitation/main.py
+++ web/invitation/main.py
@@ -2,18 +2,33 @@ import pandas as pd
 
 # 要邀请人所有交易数 和 充值总数
 
-trade = pd.read_csv("../../db/new/trade.csv", low_memory=False)
-inviter = pd.read_csv("../../db/new/invitation_user 2021-01-25.csv", low_memory=False)
-nmi = pd.read_csv("../../db/new/credit_nmi_transaction 2021-01-25.csv", low_memory=False)
-manual = pd.read_csv("../../db/new/credit_payment 2021-01-25.csv", low_memory=False)
+YEAR_MONTH = '2022-01'
 
-# check specific inviters contribute
-# inviter = inviter[inviter['inviter_id'] == 22328]
-trade = trade[trade['status'] == 40]
+inviter = pd.read_csv("../../db/new/invitation_user_2022-02-27.csv", low_memory=False)
+nmi = pd.read_csv("../../db/new/credit_nmi_transaction_2022-02-27.csv", low_memory=False)
+manual = pd.read_csv("../../db/new/credit_payment_2022-02-27.csv", low_memory=False)
+redeem = pd.read_csv("../../db/new/invitation_redeem_2022-02-27.csv", low_memory=False)
 
-a = pd.merge(inviter, trade, left_on='invitee_id', right_on='seller_id')
-a = a[['inviter_id', 'invitee_id']]
-print('total_invitee_transaction:', a.count()[0])
+inviter['create_time'] = pd.to_datetime(inviter['create_time'], unit='s')
+inviter['create_time'] = inviter['create_time'].dt.strftime('%Y-%m')
+inviter = inviter[(inviter['create_time'] == YEAR_MONTH)]
+
+nmi['create_time'] = pd.to_datetime(nmi['create_time'], unit='s')
+nmi['create_time'] = nmi['create_time'].dt.strftime('%Y-%m')
+nmi = nmi[(nmi['create_time'] == YEAR_MONTH)]
+
+manual['create_time'] = pd.to_datetime(manual['create_time'], unit='s')
+manual['create_time'] = manual['create_time'].dt.strftime('%Y-%m')
+manual = manual[(manual['create_time'] == YEAR_MONTH)]
+
+redeem['complete_time'] = pd.to_datetime(redeem['complete_time'], unit='s')
+redeem['complete_time'] = redeem['complete_time'].dt.strftime('%Y-%m')
+redeem = redeem[(redeem['complete_time'] == YEAR_MONTH) & redeem['is_complete'] == 1]
+# trade = trade[trade['status'] == 40]
+
+# a = pd.merge(inviter, trade, left_on='invitee_id', right_on='seller_id')
+# a = a[['inviter_id', 'invitee_id']]
+# print('total_invitee_transaction:', a.count()[0])
 
 #
 b = pd.merge(inviter, nmi, left_on='invitee_id', right_on='user_id')
@@ -25,8 +40,10 @@ print(c)
 b.to_csv('csv/nmi.csv')
 c.to_csv('csv/manual.csv')
 
-totalNmi = b['discount_amount'].sum()
-totalManual = c['pay_sum_discount'].sum()
+totalSendOut = round(redeem['amount'].sum(), 2)
+totalNmi = round(b['discount_amount'].sum(), 2)
+totalManual = round(c['pay_sum_discount'].sum(), 2)
+print('total send out: ', totalSendOut)
 print('total nmi: ', totalNmi)
-print('total manual:', totalManual)
+print('total CNY:', totalManual)
 print('sum:', totalNmi + totalManual)
